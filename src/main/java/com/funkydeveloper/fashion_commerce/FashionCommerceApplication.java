cc251f16c2aff2020222a907fa6e754352cbe2e3
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-27 10:10:20 +0000

    init
    project setup

---
+++ src/main/java/com/funkydeveloper/fashion_commerce/FashionCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.funkydeveloper.fashion_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funkydeveloper/fashion_commerce/FashionCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funkydeveloper.fashion_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
