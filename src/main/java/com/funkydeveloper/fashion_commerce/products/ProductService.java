cbe1dd10bf3fa3f0e7cde101655284dd27ea1ace
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-27 13:21:15 +0000

    Update product service

--- src/main/java/com/funkydeveloper/fashion_commerce/products/ProductService.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/products/ProductService.java
@@ -1,3 +1,10 @@
 package com.funkydeveloper.fashion_commerce.products;
 
-public interface ProductService { }
+import com.funkydeveloper.fashion_commerce.generics.Response;
+import com.funkydeveloper.fashion_commerce.products.requests.CreateNewProductRequest;
+import org.springframework.http.ResponseEntity;
+
+public interface ProductService {
+
+    ResponseEntity<Response<?>> createNewProduct(String id, CreateNewProductRequest request);
+}
