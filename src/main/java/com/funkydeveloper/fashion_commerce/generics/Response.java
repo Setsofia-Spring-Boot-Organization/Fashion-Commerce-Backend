0cfb6e4bfc225a8e7feda5995db594bd54731394
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-29 12:06:45 +0000

    Update: newly created product response added

--- src/main/java/com/funkydeveloper/fashion_commerce/generics/Response.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/generics/Response.java
@@ -1,5 +1,12 @@
 package com.funkydeveloper.fashion_commerce.generics;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
 public class Response<T> {
     private int status;
     private String message;
