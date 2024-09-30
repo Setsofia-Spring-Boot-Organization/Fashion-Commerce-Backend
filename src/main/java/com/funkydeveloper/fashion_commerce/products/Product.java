5281569129fc74b52be230554cfaa2c6560fd763
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-30 10:54:39 +0000

    Update: 'gender' field added to the product model

--- src/main/java/com/funkydeveloper/fashion_commerce/products/Product.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/products/Product.java
@@ -24,6 +24,7 @@ public class Product {
     private List<String> sizes;
     private List<String> colors;
     private List<String> images;
+    private List<String> genders;
     private boolean isAvailable;
 
     private String description;
