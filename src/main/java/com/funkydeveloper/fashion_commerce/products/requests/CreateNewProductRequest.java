048516b6680870c02014b79eeeeb8dbe27a4bfcd
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-01 11:01:00 +0000

    Update: filter products by the current week

--- src/main/java/com/funkydeveloper/fashion_commerce/products/requests/CreateNewProductRequest.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/products/requests/CreateNewProductRequest.java
@@ -5,6 +5,7 @@ import java.util.List;
 public record CreateNewProductRequest(
         String name,
         String price,
+        List<String> categories,
         List<String>sizes,
         List<String> colors,
         List<String> images,
