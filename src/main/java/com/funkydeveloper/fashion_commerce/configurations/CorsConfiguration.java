b497bdb591af6230edc1b944a70bd36697828878
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-01 22:21:55 +0000

    Update: get all products from last year and filter them by the gender; male, female, children

--- src/main/java/com/funkydeveloper/fashion_commerce/configurations/CorsConfiguration.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/configurations/CorsConfiguration.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
     @Value("${deployed-frontend-url}")
     private String DEPLOYED_FRONTEND_URL;
     @Value("${local-frontend-url}")
     private String LOCAL_FRONTEND_URL;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings (CorsRegistry registry) {
         registry
                 .addMapping("/api/v1/**")
                 .allowedOrigins(LOCAL_FRONTEND_URL+"/", DEPLOYED_FRONTEND_URL+"/")
