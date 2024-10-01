d7fba5c5e9604ab21d980e81b519bd8eac769473
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-01 09:03:11 +0000

    Update: search for a product by the product name

--- src/main/java/com/funkydeveloper/fashion_commerce/products/ProductRepository.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/products/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.funkydeveloper.fashion_commerce.products;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
