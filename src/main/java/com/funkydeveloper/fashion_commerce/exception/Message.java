3311aa951f15b78ae330f181955eee6b895490d6
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 10:02:46 +0000

    Update: exceptions added for creating new product types

--- src/main/java/com/funkydeveloper/fashion_commerce/exception/Message.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/exception/Message.java
@@ -4,7 +4,8 @@ public enum Message {
     CANNOT_SAVE_THE_DATA("the submitted data cannot be saved"),
     THE_FOLLOWING_FIELDS_ARE_EMPTY("the following fields are empty: "),
     THE_REQUESTED_PRODUCT_ID_IS_INCORRECT("the requested product's id is incorrect"),
-    THE_REQUESTED_GENDER_IS_INVALID("the requested gender cannot be found");
+    THE_REQUESTED_GENDER_IS_INVALID("the requested gender cannot be found"),
+    THE_PRODUCT_TYPES_COULD_NOT_BE_VERIFIED("could not verify the submitted product types");
 
     public final String label;
     Message(String label) {
