861fc5498c767c34dad6a35c82dfa661a21de2d0
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:30:35 +0000

    Update: Product type repository was added

---
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeRepository.java
@@ -0,0 +1,7 @@
+package com.funkydeveloper.fashion_commerce.productTypes;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductTypeRepository extends MongoRepository<ProductType, String> {
+
+}
