268df942564dcdf955345a048dea54e4f66a3cdf
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:44:44 +0000

    Update: new product types can be created

--- src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeService.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeService.java
@@ -1,5 +1,12 @@
 package com.funkydeveloper.fashion_commerce.productTypes;
 
+import com.funkydeveloper.fashion_commerce.generics.Response;
+import com.funkydeveloper.fashion_commerce.productTypes.responses.CreatedProductTypes;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface ProductTypeService {
 
+    ResponseEntity<Response<CreatedProductTypes>> createNewProduct(List<String> names);
 }
--- src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeServiceImpl.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeServiceImpl.java
@@ -1,11 +1,19 @@
 package com.funkydeveloper.fashion_commerce.productTypes;
 
+import com.funkydeveloper.fashion_commerce.generics.Response;
+import com.funkydeveloper.fashion_commerce.productTypes.responses.CreatedProductTypes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductTypeServiceImpl implements ProductTypeService {
 
-
+    @Override
+    public ResponseEntity<Response<CreatedProductTypes>> createNewProduct(List<String> names) {
+        return null;
+    }
 }
