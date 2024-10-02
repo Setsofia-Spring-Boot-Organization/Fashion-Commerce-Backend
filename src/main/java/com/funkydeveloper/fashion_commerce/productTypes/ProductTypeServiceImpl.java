8deb877cff94242ab45765f5ca8d464935d0012b
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:33:44 +0000

    Update: Product type service implication was added

---
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.funkydeveloper.fashion_commerce.productTypes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductTypeServiceImpl implements ProductTypeService {
+
+
+}
