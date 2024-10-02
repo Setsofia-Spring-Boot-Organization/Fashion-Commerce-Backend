653846514953f32536bf84c941228aded697cca3
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:30:15 +0000

    Update: Product type controller was added

---
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeController.java
@@ -0,0 +1,12 @@
+package com.funkydeveloper.fashion_commerce.productTypes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/product-type")
+@RequiredArgsConstructor
+public class ProductTypeController {
+
+}
