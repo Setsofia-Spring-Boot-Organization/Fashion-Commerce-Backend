646f9e39f7b29f7a4bd20d0c55921cdcdc2de09e
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:39:57 +0000

    Update: new product types can be created

--- src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeController.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeController.java
@@ -1,12 +1,25 @@
 package com.funkydeveloper.fashion_commerce.productTypes;
 
+import com.funkydeveloper.fashion_commerce.generics.Response;
+import com.funkydeveloper.fashion_commerce.productTypes.responses.CreatedProductTypes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/product-type")
 @RequiredArgsConstructor
 public class ProductTypeController {
 
+    @PostMapping
+    public ResponseEntity<Response<CreatedProductTypes>> createNewProductType(
+            @RequestBody List<String> names
+    ) {
+        return
+    }
 }
