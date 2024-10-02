67dc03041346f28dfb12574a2159526caa45b057
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-02 08:49:17 +0000

    Update: new product types can be created

--- src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeServiceImpl.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/productTypes/ProductTypeServiceImpl.java
@@ -6,14 +6,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductTypeServiceImpl implements ProductTypeService {
 
+    private final ProductTypeRepository productTypeRepository;
+
     @Override
     public ResponseEntity<Response<CreatedProductTypes>> createNewProduct(List<String> names) {
+
+        List<ProductType> productTypes = new ArrayList<>();
+
+        for (String name : names) {
+            productTypes.add(
+                    ProductType.builder()
+                            .name(name)
+                            .build()
+            );
+        }
+
+        productTypeRepository.saveAll(productTypes);
+
         return null;
     }
 }
