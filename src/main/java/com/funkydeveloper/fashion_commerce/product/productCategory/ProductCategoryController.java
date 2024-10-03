f4d57a59639ee66a811c33a7c127b2c7fbc3715a
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-03 13:03:45 +0000

    Update: create new category for products

--- src/main/java/com/funkydeveloper/fashion_commerce/product/productCategory/ProductCategoryController.java
+++ src/main/java/com/funkydeveloper/fashion_commerce/product/productCategory/ProductCategoryController.java
@@ -2,7 +2,6 @@ package com.funkydeveloper.fashion_commerce.product.productCategory;
 
 import com.funkydeveloper.fashion_commerce.generics.Response;
 import com.funkydeveloper.fashion_commerce.product.productCategory.requests.CreateNewCategory;
-import com.funkydeveloper.fashion_commerce.product.productSize.ProductSize;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
