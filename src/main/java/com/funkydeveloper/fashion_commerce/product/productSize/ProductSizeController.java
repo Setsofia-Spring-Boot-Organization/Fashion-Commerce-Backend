432d4b2e15e3eb9cc1ac9343070a8bb67b58cf2b
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-03 10:49:15 +0000

    Update: product size controller added

---
+++ src/main/java/com/funkydeveloper/fashion_commerce/product/productSize/ProductSizeController.java
@@ -0,0 +1,11 @@
+package com.funkydeveloper.fashion_commerce.product.productSize;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/product-sizes")
+public class ProductSizeController {
+
+
+}
