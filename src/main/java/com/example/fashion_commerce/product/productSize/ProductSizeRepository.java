674a4810a069f57ff1a82c9a438f77e18b8394a8
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-04 15:58:08 +0000

    Update: QueryDSL added for performing complex mongoDB queries

--- src/main/java/com/example/fashion_commerce/product/productSize/ProductSizeRepository.java
+++ src/main/java/com/example/fashion_commerce/product/productSize/ProductSizeRepository.java
@@ -1,5 +1,6 @@
 package com.example.fashion_commerce.product.productSize;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ProductSizeRepository extends MongoRepository<ProductSize, String> { }
+public interface ProductSizeRepository extends MongoRepository<ProductSize, String>, QuerydslPredicateExecutor<ProductSize> { }
