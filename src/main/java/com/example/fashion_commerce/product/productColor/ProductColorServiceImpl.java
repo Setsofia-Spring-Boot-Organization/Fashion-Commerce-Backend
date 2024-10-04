435cf37c5bac45b37de003488f5884fb90eb71fe
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-04 14:34:46 +0000

    Update: create new product color

--- src/main/java/com/example/fashion_commerce/product/productColor/ProductColorServiceImpl.java
+++ src/main/java/com/example/fashion_commerce/product/productColor/ProductColorServiceImpl.java
@@ -1,9 +1,18 @@
 package com.example.fashion_commerce.product.productColor;
 
+import com.example.fashion_commerce.generics.Response;
+import com.example.fashion_commerce.product.productColor.requests.CreateProductColor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductColorServiceImpl implements ProductColorService {
 
 
+    @Override
+    public ResponseEntity<Response<List<ProductColor>>> createNewProductColor(CreateProductColor productColor) {
+        return null;
+    }
 }
