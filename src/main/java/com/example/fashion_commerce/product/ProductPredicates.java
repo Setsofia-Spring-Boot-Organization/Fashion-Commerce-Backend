140406016f024492f22ab93be5d97a922d1e5420
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-08 13:45:29 +0000

    Update:global product filtering with the QueryDSL predicate

--- src/main/java/com/example/fashion_commerce/product/ProductPredicates.java
+++ src/main/java/com/example/fashion_commerce/product/ProductPredicates.java
@@ -50,9 +50,9 @@ public class ProductPredicates {
         }
 
         QProduct qProduct = new QProduct("product");
-        Predicate predicate = qProduct.type.equalsIgnoreCase(filter.getType());
-//                .or(qProduct.sizes.any().in(sizes)
-//                .or(qProduct.isAvailable.eq(filter.isAvailable()))
+        Predicate predicate = qProduct.type.equalsIgnoreCase(filter.getType())
+                .and(qProduct.sizes.any().in(sizes)
+                .or(qProduct.isAvailable.eq(filter.isAvailable())));
 //                .or(qProduct.categories.any().in(categories))
 //                .or(qProduct.colors.any().in(colors))
 //                .or(qProduct.price.between(filter.getStartPrice(), filter.getEndPrice())));
