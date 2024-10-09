085e38674c8cea9ee025a11658c40c83f4ef1b4b
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-09 14:06:46 +0000

    Update: Create an Order

--- src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
+++ src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
@@ -13,24 +13,34 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public ResponseEntity<Response<Order>> createOrder(CreateOrder createOrder) {
 
-        Order order = new Order(
-                new ContactInfo(
-                        createOrder.email(),
-                        createOrder.phone()
-                ),
+        ContactInfo contactInfo = createContactInfo(createOrder);
+        ShippingAddress shippingAddress = createShippingAddress(createOrder);
 
-                new ShippingAddress(
-                        createOrder.firstname(),
-                        createOrder.lastname(),
-                        createOrder.country(),
-                        createOrder.region(),
-                        createOrder.address(),
-                        createOrder.city(),
-                        createOrder.postalCode()
-                ),
+        Order order = new Order(
+                contactInfo,
+                shippingAddress,
                 createOrder.productIDs()
         );
 
         return null;
+    }
+
+    private ContactInfo createContactInfo(CreateOrder createOrder) {
+        return new ContactInfo(
+                createOrder.email(),
+                createOrder.phone()
+        );
+    }
+
+    private ShippingAddress createShippingAddress(CreateOrder createOrder) {
+        return new ShippingAddress(
+                createOrder.firstname(),
+                createOrder.lastname(),
+                createOrder.country(),
+                createOrder.region(),
+                createOrder.address(),
+                createOrder.city(),
+                createOrder.postalCode()
+        );
     }
 }
