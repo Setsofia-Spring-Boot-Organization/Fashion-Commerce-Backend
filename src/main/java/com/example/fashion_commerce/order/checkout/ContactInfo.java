499b952f43480bb2872a2f2ea97f27c6354e3f50
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-09 14:33:03 +0000

    Update: transaction management was added during data creation to roll back all processes, when one fails

--- src/main/java/com/example/fashion_commerce/order/checkout/ContactInfo.java
+++ src/main/java/com/example/fashion_commerce/order/checkout/ContactInfo.java
@@ -1,6 +1,50 @@
 package com.example.fashion_commerce.order.checkout;
 
-public record ContactInfo(
-    String email,
-    String phone
-) { }
+import java.util.Objects;
+
+public class ContactInfo {
+    private String email;
+    private String phone;
+
+    public ContactInfo(String email, String phone) {
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactInfo that = (ContactInfo) o;
+        return Objects.equals(email, that.email) && Objects.equals(phone, that.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
