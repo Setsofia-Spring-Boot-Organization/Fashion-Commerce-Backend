d00a781cae537ee4b4f0ec18468c5f1428dd8c3d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-10 13:54:34 +0000

    Update: code optimized

--- src/main/java/com/example/fashion_commerce/order/OrderController.java
+++ src/main/java/com/example/fashion_commerce/order/OrderController.java
@@ -2,11 +2,11 @@ package com.example.fashion_commerce.order;
 
 import com.example.fashion_commerce.generics.Response;
 import com.example.fashion_commerce.order.requests.CreateOrder;
+import com.example.fashion_commerce.order.requests.OrderStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/order")
@@ -25,5 +25,13 @@ public class OrderController {
             @RequestBody CreateOrder order
     ) {
         return orderService.createOrder(order);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Response<List<Order>>> getOrders(
+            @RequestParam OrderStatus status
+    ) {
+        return orderService.getOrders(status);
     }
 }
