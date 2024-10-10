4bc0c2b3ac75a96ced87cc0d8711f9d490b46b2d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-10 15:35:21 +0000

    Update: get orders by their status

--- src/main/java/com/example/fashion_commerce/exception/Error.java
+++ src/main/java/com/example/fashion_commerce/exception/Error.java
@@ -7,7 +7,8 @@ public enum Error {
     INVALID_GENDER("invalid gender"),
     INVALID_PRODUCT_TYPES("invalid product type(s)"),
     FILE_SIZE_TOO_LARGE("file size too large"),
-    INVALID_PRODUCT_IDS("invalid product id(s)");
+    INVALID_PRODUCT_IDS("invalid product id(s)"),
+    INVALID_ORDER_STATUS("invalid order status");
 
     public final String label;
     Error(String label) {
