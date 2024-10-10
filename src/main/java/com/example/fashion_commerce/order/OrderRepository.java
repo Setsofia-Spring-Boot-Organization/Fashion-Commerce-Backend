940bc2a67a76bdc3a2766befbdcf8087c0772d50
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-10 14:19:12 +0000

    Update: get orders by their status

--- src/main/java/com/example/fashion_commerce/order/OrderRepository.java
+++ src/main/java/com/example/fashion_commerce/order/OrderRepository.java
@@ -1,6 +1,18 @@
 package com.example.fashion_commerce.order;
 
+import com.querydsl.core.types.Predicate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import com.example.fashion_commerce.order.requests.RequestOrderStatus;
 
-public interface OrderRepository extends MongoRepository<Order, String>, QuerydslPredicateExecutor<Order> { }
+import java.util.List;
+
+public interface OrderRepository extends MongoRepository<Order, String>, QuerydslPredicateExecutor<Order> {
+
+    default List<Order> findOrderByStatus(RequestOrderStatus orderStatus) {
+        QOrder qOrder = new QOrder("order");
+        Predicate predicate = qOrder.orderStatus.eq(orderStatus.getStatus()) ;
+
+        return (List<Order>) findAll(predicate);
+    }
+}
