e6c96300179ff7dde60b19f8af420447c28331b5
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-10 14:25:06 +0000

    Update: get orders by their status

--- src/main/java/com/example/fashion_commerce/order/OrderService.java
+++ src/main/java/com/example/fashion_commerce/order/OrderService.java
@@ -2,7 +2,7 @@ package com.example.fashion_commerce.order;
 
 import com.example.fashion_commerce.generics.Response;
 import com.example.fashion_commerce.order.requests.CreateOrder;
-import com.example.fashion_commerce.order.requests.OrderStatus;
+import com.example.fashion_commerce.order.requests.RequestOrderStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -10,5 +10,5 @@ import java.util.List;
 public interface OrderService {
     ResponseEntity<Response<Order>> createOrder(CreateOrder order);
 
-    ResponseEntity<Response<List<Order>>> getOrders(OrderStatus status);
+    ResponseEntity<Response<List<Order>>> getOrders(boolean all, RequestOrderStatus status);
 }
