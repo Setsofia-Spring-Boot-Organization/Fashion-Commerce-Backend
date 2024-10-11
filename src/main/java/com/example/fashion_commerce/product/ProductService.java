5adf3778ff7bec5615920a84ff0a8d222cd45651
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-11 07:36:57 +0000

    Update: get all filter options

--- src/main/java/com/example/fashion_commerce/product/ProductService.java
+++ src/main/java/com/example/fashion_commerce/product/ProductService.java
@@ -3,10 +3,7 @@ package com.example.fashion_commerce.product;
 import com.example.fashion_commerce.generics.Response;
 import com.example.fashion_commerce.product.requests.CreateNewProductRequest;
 import com.example.fashion_commerce.product.requests.FilterProducts;
-import com.example.fashion_commerce.product.responses.AllProductsRes;
-import com.example.fashion_commerce.product.responses.CreatedProductRes;
-import com.example.fashion_commerce.product.responses.GetNewCollectionRes;
-import com.example.fashion_commerce.product.responses.ThisWeekProductsRes;
+import com.example.fashion_commerce.product.responses.*;
 import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
@@ -42,4 +39,6 @@ public interface ProductService {
     ResponseEntity<Response<AllProductsRes>> getProducts();
 
     ResponseEntity<Response<List<Product>>> filterAllProducts(FilterProducts filter);
+
+    ResponseEntity<Response<FilterOptions>> getFilterOptions();
 }
