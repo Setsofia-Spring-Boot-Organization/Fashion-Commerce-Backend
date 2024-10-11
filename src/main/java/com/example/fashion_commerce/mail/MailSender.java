74fe3b5e9b47a407f30adec5faee744001ad77bf
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-11 11:01:15 +0000

    Update: application.properties updated

--- src/main/java/com/example/fashion_commerce/mail/MailSender.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSender.java
@@ -1,9 +1,17 @@
 package com.example.fashion_commerce.mail;
 
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailSender {
+
+    private final JavaMailSender mailSender;
+
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
 
     
 }
