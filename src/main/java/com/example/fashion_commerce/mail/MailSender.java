849f56bfbfc776004a96c2a02d0dd29e27fe43ea
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-11 11:08:27 +0000

    Update: application.properties updated

--- src/main/java/com/example/fashion_commerce/mail/MailSender.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSender.java
@@ -2,5 +2,5 @@ package com.example.fashion_commerce.mail;
 
 public interface MailSender {
 
-    void sendMail(String to, String subject, String content);
+    void sendMail(String to, String subject, String text);
 }
