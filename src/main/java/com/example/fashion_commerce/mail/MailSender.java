0a9658a163b4dfaabfe0f0e739547ef7522a01eb
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-12 14:20:51 +0000

    Update: Added thymeleaf

--- src/main/java/com/example/fashion_commerce/mail/MailSender.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSender.java
@@ -1,6 +1,8 @@
 package com.example.fashion_commerce.mail;
 
+import java.util.Map;
+
 public interface MailSender {
 
-    void sendMail(String to, String subject, String text);
+    void sendMail(String to, String subject, Map<String, Object> variables, String template);
 }
