6293d8ab1c8bd18d8ad3f14055032338c0cc4f45
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-12 14:38:36 +0000

    Update: MimeMesssage was added

--- src/main/java/com/example/fashion_commerce/mail/MailSenderImpl.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSenderImpl.java
@@ -3,7 +3,6 @@ package com.example.fashion_commerce.mail;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -36,14 +35,12 @@ public class MailSenderImpl implements MailSender {
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        messageHelper.setPriority(1);
+        messageHelper.setSubject(subject);
+        messageHelper.setFrom(MAIL_SENDER);
+        messageHelper.setTo(to);
+        messageHelper.setText(text, true);
 
-
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom(MAIL_SENDER);
-        simpleMailMessage.setTo(to);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(text);
-
-        mailSender.send(simpleMailMessage);
+        mailSender.send(mimeMessage);
     }
 }
