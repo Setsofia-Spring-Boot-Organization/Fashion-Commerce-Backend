e1903a92a4126e4f9a77f0b840e27a3b6679a0f7
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-12 14:38:17 +0000

    Update: MimeMesssage was added

--- src/main/java/com/example/fashion_commerce/mail/MailSender.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSender.java
@@ -1,8 +1,10 @@
 package com.example.fashion_commerce.mail;
 
+import jakarta.mail.MessagingException;
+
 import java.util.Map;
 
 public interface MailSender {
 
-    void sendMail(String to, String subject, Map<String, Object> variables, String template);
+    void sendMail(String to, String subject, Map<String, Object> variables, String template) throws MessagingException;
 }
