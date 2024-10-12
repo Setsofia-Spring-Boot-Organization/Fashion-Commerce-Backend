a1e0597d39c39c60d957435a5bbeec9c6e71c607
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-12 14:44:28 +0000

    Update: MimeMesssage was added

--- src/main/java/com/example/fashion_commerce/mail/MailSenderImpl.java
+++ src/main/java/com/example/fashion_commerce/mail/MailSenderImpl.java
@@ -3,6 +3,7 @@ package com.example.fashion_commerce.mail;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -26,21 +27,27 @@ public class MailSenderImpl implements MailSender {
     @Override
     public void sendMail(String to, String subject, Map<String, Object> variables, String template) throws MessagingException {
 
-        TemplateEngine templateEngine = new TemplateEngine();
-        Context context = new Context();
+//        TemplateEngine templateEngine = new TemplateEngine();
+//        Context context = new Context();
+//
+//        context.setVariables(variables);
 
-        context.setVariables(variables);
+//        String text = templateEngine.process(template, context);
 
-        String text = templateEngine.process(template, context);
+//        MimeMessage mimeMessage = mailSender.createMimeMessage();
+//        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+//        messageHelper.setPriority(1);
+//        messageHelper.setSubject(subject);
+//        messageHelper.setFrom(MAIL_SENDER);
+//        messageHelper.setTo(to);
+//        messageHelper.setText(text, true);
 
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-        messageHelper.setPriority(1);
-        messageHelper.setSubject(subject);
-        messageHelper.setFrom(MAIL_SENDER);
-        messageHelper.setTo(to);
-        messageHelper.setText(text, true);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(MAIL_SENDER);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(template);
 
-        mailSender.send(mimeMessage);
+        mailSender.send(simpleMailMessage);
     }
 }
