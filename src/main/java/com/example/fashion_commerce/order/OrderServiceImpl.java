c577a35067df5d87e63a585cf22f5af3f0613183
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-14 06:35:10 +0000

    Update: order confirmation email template was added

--- src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
+++ src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
@@ -53,7 +53,7 @@ public class OrderServiceImpl implements OrderService {
                     order.getContactInfo().getEmail(),
                     "Thank you for your order 🌹🌹",
                     variables,
-                    "SuccessfulOrderFeedback"
+                    "OrderConfirmation"
             );
 
             Response<Order> orderResponse = new Response<>(
