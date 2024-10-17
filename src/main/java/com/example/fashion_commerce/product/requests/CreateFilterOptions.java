ed2a980db8b1f79307e01f04e9e180e6f2909b5f
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-17 11:29:52 +0000

    Update: update a product and add all filter options when the product is created

--- src/main/java/com/example/fashion_commerce/product/requests/CreateFilterOptions.java
+++ src/main/java/com/example/fashion_commerce/product/requests/CreateFilterOptions.java
@@ -1,13 +1,10 @@
 package com.example.fashion_commerce.product.requests;
 
-import com.example.fashion_commerce.product.productCategory.requests.CreateNewCategory;
-import com.example.fashion_commerce.product.productColor.requests.CreateProductColor;
-import com.example.fashion_commerce.product.productSize.requests.CreateProductSize;
-import com.example.fashion_commerce.product.productType.requests.CreateProductType;
+import java.util.List;
 
 public record CreateFilterOptions(
-        CreateNewCategory category,
-        CreateProductColor color,
-        CreateProductSize size,
-        CreateProductType type
+        List<String> categories,
+        List<String> colors,
+        List<String> sizes,
+        List<String> types
 ) { }
