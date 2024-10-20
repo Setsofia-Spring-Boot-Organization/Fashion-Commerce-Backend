e483c53190eb26abbf6c535a6a7df2f433272178
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-20 23:32:51 +0000

    Update: update a product and add all filter options when the product is created

--- src/main/java/com/example/fashion_commerce/order/requests/OrderProductsIds.java
+++ src/main/java/com/example/fashion_commerce/order/requests/OrderProductsIds.java
@@ -1,6 +1,50 @@
 package com.example.fashion_commerce.order.requests;
 
-public record OrderProductsIds(
-        String id,
-        int quantity
-) { }
+import java.util.Objects;
+
+public class OrderProductsIds {
+        String id;
+        int quantity;
+
+    public OrderProductsIds(String id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderProductsIds that = (OrderProductsIds) o;
+        return quantity == that.quantity && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderProductsIds{" +
+                "id='" + id + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
