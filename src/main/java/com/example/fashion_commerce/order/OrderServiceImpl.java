390c4ce1f5c8f05463107e0370b33bb2300a30e3
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-24 13:30:29 +0000

    Update:

--- src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
+++ src/main/java/com/example/fashion_commerce/order/OrderServiceImpl.java
@@ -20,6 +20,7 @@ import com.example.fashion_commerce.order.requests.RequestOrderStatus;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -46,14 +47,22 @@ public class OrderServiceImpl implements OrderService {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             String username = order.getShippingAddress().getFirstname() + " " + order.getShippingAddress().getLastname(); // combine the username
             List<Product> products = order.getProducts().stream().map(OrderProducts::getProduct).toList();
-            double subtotalPrice = products.stream().map(Product::getPrice).toList().stream().mapToDouble(Double::doubleValue).sum();
+//            double subtotalPrice = products.stream().map(Product::getPrice).toList().stream().mapToDouble(Double::doubleValue).sum();
 
+           List<Double> subtotalPrice = new ArrayList<>();
+
+            for (OrderProducts product : order.getProducts()) {
+                double price = product.getProduct().getPrice();
+                int quantity = product.getQuantity();
+
+                subtotalPrice.add((price * quantity));
+            }
 
             // calculate the costs
             double shippingCost = order.getShippingAddress().getShippingCost();
             double tax = order.getShippingAddress().getTax();
             double tempTotalPrice = shippingCost + tax;
-            double totalPrice = (subtotalPrice + tempTotalPrice);
+            double totalPrice = (subtotalPrice.stream().mapToDouble(Double::doubleValue).sum() + tempTotalPrice);
 
             Map<String, Object> variables = Map.ofEntries(
                     Map.entry("username", username),
