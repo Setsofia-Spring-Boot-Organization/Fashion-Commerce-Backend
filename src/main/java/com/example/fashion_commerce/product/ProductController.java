767b294f1b6d7f72ba805e2502ae17d8178e16db
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-10-31 17:26:51 +0000

    Update:

--- src/main/java/com/example/fashion_commerce/product/ProductController.java
+++ src/main/java/com/example/fashion_commerce/product/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/product")
