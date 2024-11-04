67ffe2b937a46421af5ef1508dc551fb52391764
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-04 14:02:58 +0000

    Update:

--- src/main/java/com/example/fashion_commerce/order/OrderRepository.java
+++ src/main/java/com/example/fashion_commerce/order/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import com.example.fashion_commerce.order.requests.RequestOrderStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends MongoRepository<Order, String>, QuerydslPredicateExecutor<Order> {
@@ -12,6 +13,15 @@ public interface OrderRepository extends MongoRepository<Order, String>, Queryds
     default List<Order> findOrderByStatus(RequestOrderStatus orderStatus) {
         QOrder qOrder = new QOrder("orders");
         Predicate predicate = qOrder.orderStatus.eq(OrderStatus.valueOf(orderStatus.getStatus().toUpperCase())) ;
+
+        return (List<Order>) findAll(predicate);
+    }
+
+    default List<Order> findOrderByDateAndStatus(LocalDateTime date, String status) {
+        QOrder qOrder = new QOrder("orders");
+        Predicate predicate = qOrder.dateCreated.after(date).and(
+                qOrder.orderStatus.eq(OrderStatus.valueOf(status.toUpperCase()))
+        );
 
         return (List<Order>) findAll(predicate);
     }
