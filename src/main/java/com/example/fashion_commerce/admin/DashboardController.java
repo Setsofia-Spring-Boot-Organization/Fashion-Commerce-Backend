f7df33f596491b9ff934dfd80ff2ff454546b5fe
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-04 13:43:41 +0000

    Update:

--- src/main/java/com/example/fashion_commerce/admin/DashboardController.java
+++ src/main/java/com/example/fashion_commerce/admin/DashboardController.java
@@ -1,11 +1,24 @@
 package com.example.fashion_commerce.admin;
 
+import com.example.fashion_commerce.admin.responses.OrderAnalytics;
+import com.example.fashion_commerce.generics.Response;
+import com.example.fashion_commerce.order.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/admin/dashboard")
 public class DashboardController {
+    private final OrderService orderService;
 
-    
+    public DashboardController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("order/analytics")
+    public ResponseEntity<Response<OrderAnalytics>> getOrderAnalytics() {
+        return orderService.getOrderAnalytics();
+    }
 }
