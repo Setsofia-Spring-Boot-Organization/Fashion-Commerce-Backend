3fa46d932f4a88abd153513e864ca0f43ce4b55d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-04 13:37:04 +0000

    Update:

---
+++ src/main/java/com/example/fashion_commerce/admin/DashboardController.java
@@ -0,0 +1,11 @@
+package com.example.fashion_commerce.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/admin/dashboard")
+public class DashboardController {
+
+
+}
